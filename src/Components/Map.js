c8094179b2c1dbc6849a2365663260bf58f1088e
unknown <gabija.zvirblyte@gmail.com>
2020-09-02 14:49:07 +0300

    added functions to calculate distance

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -19,16 +19,27 @@ class Map extends React.Component {
         }
     }
 
-
     handleClick = (e) => {
-        console.log(e.lngLat);
         this.setState({
-            point: [...this.state.point, [e.lngLat[0], e.lngLat[1]]],
-            
+            point: [...this.state.point, [e.lngLat[0], e.lngLat[1]]]
         })
+        let distance = this.getFullDistance();
+        this.setState({ distance })
     }
 
-    getDistance = (mk1, mk2) => {
+    getFullDistance = () => {
+        const points = this.state.point;
+        let distance = 0;
+        for (let i = 1; i < points.length; i++) {
+            if (points[i + 1] !== undefined) {
+                distance += this.calcDistance(points[i], points[i + 1])
+            }
+        }
+        // console.log(`DISTANCE ${distance.toFixed(2)} KM`)
+        return distance;
+    }
+
+    calcDistance = (mk1, mk2) => {
         const R = 6371.0710; // Radius of the Earth in kilometres
         const rlat1 = mk1[1] * (Math.PI / 180); // Convert degrees to radians
         const rlat2 = mk2[1] * (Math.PI / 180); // Convert degrees to radians
@@ -36,12 +47,12 @@ class Map extends React.Component {
         const difflon = (mk2[0] - mk1[0]) * (Math.PI / 180); // Radian difference (longitudes)
         const distance = 2 * R * Math.asin(Math.sqrt(Math.sin(difflat / 2) * Math.sin(difflat / 2) + Math.cos(rlat1) * Math.cos(rlat2) * Math.sin(difflon / 2) * Math.sin(difflon / 2)));
         return distance;
-        //console.log('DISTANCE ' + distance);
     }
 
     render() {
         const markers = this.state.point.map(point =>
             <Marker
+                key={point}
                 latitude={point[1]} longitude={point[0]} offsetLeft={-20} offsetTop={-10}>
                 <img className="marker" src="marker.svg" alt="marker" />
             </Marker>
@@ -62,6 +73,7 @@ class Map extends React.Component {
                     <div style={{ position: 'absolute', left: 10, top: 10 }}>
                         <NavigationControl />
                     </div>
+                    <button onClick={this.getFullDistance}>GET DISTANCE</button>
                 </ReactMapGl>
             </div>
         )
