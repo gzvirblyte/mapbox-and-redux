17feb1f0d59c6979c925f4b81322800a20712c8a
unknown <gabija.zvirblyte@gmail.com>
2020-09-02 13:57:16 +0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Map from './Components/Map';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Interactive Vilnius map</h1>
+    <Map />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Map.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import ReactMapGl, { NavigationControl, Marker } from 'react-map-gl';
+
+const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_TOKEN;
+
+class Map extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            viewport: {
+                latitude: 54.6872,
+                longitude: 25.2797,
+                zoom: 12,
+                width: '100vw',
+                height: '100vh',
+            },
+            point: [[0, 0]],
+            distance: 0
+        }
+    }
+
+
+    handleClick = (e) => {
+        console.log(e.lngLat);
+        this.setState({
+            point: [...this.state.point, [e.lngLat[0], e.lngLat[1]]],
+            
+        })
+    }
+
+    getDistance = (mk1, mk2) => {
+        const R = 6371.0710; // Radius of the Earth in kilometres
+        const rlat1 = mk1[1] * (Math.PI / 180); // Convert degrees to radians
+        const rlat2 = mk2[1] * (Math.PI / 180); // Convert degrees to radians
+        const difflat = rlat2 - rlat1; // Radian difference (latitudes)
+        const difflon = (mk2[0] - mk1[0]) * (Math.PI / 180); // Radian difference (longitudes)
+        const distance = 2 * R * Math.asin(Math.sqrt(Math.sin(difflat / 2) * Math.sin(difflat / 2) + Math.cos(rlat1) * Math.cos(rlat2) * Math.sin(difflon / 2) * Math.sin(difflon / 2)));
+        return distance;
+        //console.log('DISTANCE ' + distance);
+    }
+
+    render() {
+        const markers = this.state.point.map(point =>
+            <Marker
+                latitude={point[1]} longitude={point[0]} offsetLeft={-20} offsetTop={-10}>
+                <img className="marker" src="marker.svg" alt="marker" />
+            </Marker>
+        );
+
+        return (
+            <div>
+                <ReactMapGl
+                    {...this.state.viewport}
+                    mapboxApiAccessToken={MAPBOX_TOKEN}
+                    mapStyle="mapbox://styles/gabijaz/ckejp02ko5y7019pt8snuj3lz"
+                    onViewportChange={(viewport) => this.setState({ viewport })}
+                    onClick={this.handleClick}
+                >
+                    {markers}
+
+
+                    <div style={{ position: 'absolute', left: 10, top: 10 }}>
+                        <NavigationControl />
+                    </div>
+                </ReactMapGl>
+            </div>
+        )
+    }
+}
+
+export default Map;
